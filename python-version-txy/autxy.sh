5e0ec832a8eecb215171d18bf8d87420dec7e9fc
zyt <zyt4321@qq.com>
2018-11-11 22:05:00 +0800

    增加python版本的腾讯云DNS修改

--- python-version-txy/autxy.sh
+++ python-version-txy/autxy.sh
@@ -15,7 +15,7 @@ echo $CERTBOT_DOMAIN"_acme-challenge"$CERTBOT_VALIDATION
 # 第一个参数：需要为那个域名设置 DNS 记录
 # 第二个参数：需要为具体那个 RR 设置
 # 第三个参数: letsencrypt 动态传递的 RR 值
-python3  $PATH"/txydns36.py"  $CERTBOT_DOMAIN "_acme-challenge"  $CERTBOT_VALIDATION >"/var/log/certdebug.log"
+/usr/bin/python3  $PATH"/txydns36.py"  $CERTBOT_DOMAIN "_acme-challenge"  $CERTBOT_VALIDATION >"/var/log/certdebug.log"
 
 # DNS TXT 记录刷新时间
 /bin/sleep 20
